55cdf77e96bf16b1ccca204fa3c2b41d7a86926b
Paavan Vasudev <paavanv@gmail.com>
2018-02-18 14:28:33 -0800

    Added a method to generate IntentRequest to LocalSkillRunner.

--- src/main/java/net/paavan/audioplayerskill/LocalSkillRunner.java
+++ src/main/java/net/paavan/audioplayerskill/LocalSkillRunner.java
@@ -2,8 +2,10 @@ package net.paavan.audioplayerskill;
 
 import com.amazon.speech.json.SpeechletRequestEnvelope;
 import com.amazon.speech.json.SpeechletRequestModule;
+import com.amazon.speech.slu.Intent;
 import com.amazon.speech.speechlet.Application;
 import com.amazon.speech.speechlet.Context;
+import com.amazon.speech.speechlet.IntentRequest;
 import com.amazon.speech.speechlet.LaunchRequest;
 import com.amazon.speech.speechlet.Session;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -24,6 +26,7 @@ import java.util.Locale;
 @Slf4j
 public class LocalSkillRunner {
     private static final String SKILL_ID = "amzn1.ask.skill.22b48f9a-0946-47fb-9a4c-fc731b7a398e";
+
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper() {{
         registerModule(new SpeechletRequestModule());
     }};
@@ -31,7 +34,48 @@ public class LocalSkillRunner {
     public static void main(String[] args) throws IOException {
         AudioPlayerSpeechletRequestStreamHandler handler = new AudioPlayerSpeechletRequestStreamHandler();
 
-        SpeechletRequestEnvelope<LaunchRequest> launchRequestEnvelope = SpeechletRequestEnvelope.<LaunchRequest>builder()
+        SpeechletRequestEnvelope<LaunchRequest> launchRequestEnvelope = getLaunchRequestSpeechletRequestEnvelope();
+        SpeechletRequestEnvelope<IntentRequest> intentRequestEnvelope = getIntentRequestSpeechletRequestEnvelope();
+
+        InputStream inputStream = new ByteArrayInputStream(OBJECT_MAPPER.writeValueAsBytes(launchRequestEnvelope));
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        handler.handleRequest(inputStream, outputStream, null);
+
+        log.info(outputStream.toString());
+    }
+
+    // --------------
+    // Helper Methods
+
+    /**
+     * Generates and returns a {@link SpeechletRequestEnvelope<LaunchRequest>} using fabricated data.
+     *
+     * @return a {@link SpeechletRequestEnvelope<LaunchRequest>} using fabricated data.
+     */
+    private static SpeechletRequestEnvelope<LaunchRequest> getLaunchRequestSpeechletRequestEnvelope() {
+        return SpeechletRequestEnvelope.<LaunchRequest>builder()
+                    .withVersion("1.0")
+                    .withSession(Session.builder()
+                            .withIsNew(true)
+                            .withSessionId("someSessionId")
+                            .withApplication(new Application(SKILL_ID))
+                            .build())
+                    .withContext(Context.builder().build())
+                    .withRequest(LaunchRequest.builder()
+                            .withLocale(Locale.US)
+                            .withRequestId("someRequestId")
+                            .withTimestamp(new Date())
+                            .build())
+                    .build();
+    }
+
+    /**
+     * Generates and returns a {@link SpeechletRequestEnvelope<IntentRequest>} using fabricated data.
+     *
+     * @return a {@link SpeechletRequestEnvelope<IntentRequest>} using fabricated data.
+     */
+    private static SpeechletRequestEnvelope<IntentRequest> getIntentRequestSpeechletRequestEnvelope() {
+        return SpeechletRequestEnvelope.<IntentRequest>builder()
                 .withVersion("1.0")
                 .withSession(Session.builder()
                         .withIsNew(true)
@@ -39,17 +83,14 @@ public class LocalSkillRunner {
                         .withApplication(new Application(SKILL_ID))
                         .build())
                 .withContext(Context.builder().build())
-                .withRequest(LaunchRequest.builder()
+                .withRequest(IntentRequest.builder()
                         .withLocale(Locale.US)
                         .withRequestId("someRequestId")
                         .withTimestamp(new Date())
+                        .withIntent(Intent.builder()
+                                .withName("PlayCokeStudioIntent")
+                                .build())
                         .build())
                 .build();
-
-        InputStream inputStream = new ByteArrayInputStream(OBJECT_MAPPER.writeValueAsBytes(launchRequestEnvelope));
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        handler.handleRequest(inputStream, outputStream, null);
-
-        log.info(outputStream.toString());
     }
 }
